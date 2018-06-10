32e4a13871b5c66e537a78bf49ffc96b06b11ce1
Sergio Costas <raster@rastersoft.com>
2018-06-10 17:35:26 +0200

    Updated to panther launcher

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
 /* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
 
 /***************************************************
- *           SlingShot for Gnome Shell             *
+ *       Panther-launcher for Gnome Shell          *
  *                                                 *
- * Allows to launch SlingShot launcher from        *
- * ElementaryOS under Gnome Shell                  *
+ * Allows to use Panther launcher (an slingshot    *
+ * fork from ElementaryOS) under Gnome Shell       *
  *                                                 *
  * Created by rastersoft, and distributed under    *
  * GPLv2 or later license.                         *
@@ -16,9 +16,14 @@
      1: First public version
      2: Cleaned the code
      3: Vertical align of the Applications text (thanks to Ov3rlo4d)
+     4: Changed for Panther-launcher
 
 */
 const Clutter = imports.gi.Clutter;
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const Meta = imports.gi.Meta;
+const Shell = imports.gi.Shell;
 
 const Lang = imports.lang;
 const St = imports.gi.St;
@@ -27,20 +32,29 @@ const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const Util = imports.misc.util;
 
+const MyIface = '<node>\
+<interface name="com.rastersoft.panther.remotecontrol">\
+  <method name="DoShow" />\
+  <method name="DoPing" >\
+    <arg name="n" direction="in" type="i"/>\
+    <arg name="response" direction="out" type="i"/>\
+  </method>\
+</interface>\
+</node>';
+
+const MyProxy = Gio.DBusProxy.makeProxyWrapper(MyIface);
+
 const LauncherButton = new Lang.Class({
-    Name: 'SlingShot_Gnome.LauncherButton',
+    Name: 'Panther_Gnome.LauncherButton',
     Extends: PanelMenu.Button,
 
     _init: function() {
 
-        this.parent(0.0,'SlingShot_Gnome');
+        this.parent(0.0,'Panther_Gnome');
         this.actor.add_style_class_name('panel-status-button');
         this._box = new St.BoxLayout({ style_class: 'panel-status-button-box' });
         this.actor.add_actor(this._box);
 
-        /*this.buttonIcon = new St.Icon({ gicon: null, style_class: 'system-status-icon' });
-        this.buttonIcon.icon_name='start-here';
-        this._box.add_actor(this.buttonIcon);*/
         this.buttonLabel = new St.Label({ text: _("Applications"),
                                               y_expand: true,
                                               y_align: Clutter.ActorAlign.CENTER });
@@ -49,18 +63,24 @@ const LauncherButton = new Lang.Class({
 
         this._setActivitiesNoVisible(true);
 
-        // When the user clicks in the Application button, launch slingshot-launcher
+        // When the user clicks in the Application button, launch panther-launcher
         // to show it
-        this.actor.connect('button-release-event', function(element,event) {
-            Util.spawn(['slingshot-launcher'])
-        });
-        // At startup, prelaunch slingshot to make it faster the first time the
-        // user wants it (slingshot will remain in background, and the new
+        this.actor.connect('button-release-event', Lang.bind(this,this.launch_function));
+
+        // At startup, prelaunch panther to make it faster the first time the
+        // user wants it (panther will remain in background, and the new
         // launches will just instruct it to show, instead of being reloaded
-        Util.spawn(['slingshot-launcher', '-s'])
+        // Util.spawn(['panther_launcher', '-s']); // now just use a call using DBus, which will activate it automagically
+        let instance = new MyProxy(Gio.DBus.session, 'com.rastersoft.panther.remotecontrol','/com/rastersoft/panther/remotecontrol');
+        instance.DoPingSync(0);
+
+        let mode = Shell.ActionMode ? Shell.ActionMode.NORMAL : Shell.KeyBindingMode.ALL;
+        let flags = Meta.KeyBindingFlags.NONE;
+        Main.wm.addKeybinding("show-keybinding", new Gio.Settings({schema: 'org.rastersoft.panther'}),flags,mode, Lang.bind(this, this.launch_function));
     },
 
     destroy: function() {
+        Main.wm.removeKeybinding("show-keybinding");
         this._setActivitiesNoVisible(false);
         this.parent();
     },
@@ -78,17 +98,21 @@ const LauncherButton = new Lang.Class({
         }
     },
 
+    launch_function: function () {
+        let instance = new MyProxy(Gio.DBus.session, 'com.rastersoft.panther.remotecontrol','/com/rastersoft/panther/remotecontrol');
+        instance.DoShowSync();
+    },
 });
 
-let SlingShotButton;
+let PantherButton;
 
 function enable() {
-    SlingShotButton = new LauncherButton();
-    Main.panel.addToStatusArea('slingshot-menu', SlingShotButton, 0, 'left');
+    PantherButton = new LauncherButton();
+    Main.panel.addToStatusArea('panther-menu', PantherButton, 0, 'left');
 }
 
 function disable() {
-    SlingShotButton.destroy();
+    PantherButton.destroy();
 }
 
 function init(extensionMeta) {
